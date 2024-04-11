11bdacd35e51a3e973f48b049771d9e4987f5b70
Saumya Bhatt <108668549+SaumyaBhatt106@users.noreply.github.com>
2024-04-11 21:23:24 +0530

    Creating topics on startup

--- kafka/setup/entrypoint.sh
+++ kafka/setup/entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Create the topic "web-events"
-kafka-topics --create --bootstrap-server broker1:9092 --topic web-events --partitions 3 --replication-factor 1
+# Create the topic "click-events"
+kafka-topics --create --bootstrap-server broker1:9092 --topic click-events --partitions 3 --replication-factor 1
+
+# Create the topic "order-events"
+kafka-topics --create --bootstrap-server broker1:9092 --topic order-events --partitions 3 --replication-factor 1
 
 wait_for_schema_registry() {
     until curl -sSf -X GET http://schema-registry:8081/subjects >/dev/null; do
@@ -13,7 +16,12 @@ wait_for_schema_registry() {
 # Call the function to wait for schema registry
 wait_for_schema_registry
 
-# Register the schema "web-vevents"
+# Register the schema "click-events"
 curl -X POST -H "Content-Type: application/vnd.schemaregistry.v1+json" \
-    --data '{"schema": "{\"type\":\"record\",\"name\":\"event\",\"fields\":[{\"name\":\"sub_type\",\"type\":\"string\"},{\"name\":\"ip\",\"type\":\"string\"},{\"name\":\"client_timestamp\",\"type\":\"long\"},{\"name\":\"data\",\"type\":\"string\"}]}" }' \
-    http://schema-registry:8081/subjects/web-events-value/versions
+    --data '{"schema": "{\"type\":\"record\",\"name\":\"click_event\",\"fields\":[{\"name\":\"sub_type\",\"type\":\"string\"},{\"name\":\"ip\",\"type\":\"string\"},{\"name\":\"client_timestamp\",\"type\":\"long\"},{\"name\":\"data\",\"type\":\"string\"}]}" }' \
+    http://schema-registry:8081/subjects/click-events-value/versions
+
+# Register the schema "order-events"
+curl -X POST -H "Content-Type: application/vnd.schemaregistry.v1+json" \
+    --data '{"schema": "{\"type\":\"record\",\"name\":\"order_event\",\"fields\":[{\"name\":\"sub_type\",\"type\":\"string\"},{\"name\":\"ip\",\"type\":\"string\"},{\"name\":\"client_timestamp\",\"type\":\"long\"},{\"name\":\"data\",\"type\":\"string\"}]}" }' \
+    http://schema-registry:8081/subjects/order-events-value/versions
